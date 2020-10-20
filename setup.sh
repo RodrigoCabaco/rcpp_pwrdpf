d3cc425a25cee0f1ba512b590fbdbdb12ee97d38
Rodrigo Miguel Nunes Cabaço <rodrigonunescabaco@gmail.com>
2020-10-20 15:03:53 +0100

    Update

---
+++ setup.sh
@@ -0,0 +1,25 @@
+wget -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget https://packages.microsoft.com/config/debian/9/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
+
+
+  sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-3.1
+
+sudo apt-get install -y dotnet-runtime-3.1
+
+dotnet run
+
+cp bin/Debug/netcoreapp3.1/rcpp rcpp
+cp bin/Debug/netcoreapp3.1/TextToAsciiArt.dll TextToAsciiArt.dll
+cp bin/Debug/netcoreapp3.1/rcpp.dll rcpp.dll 
