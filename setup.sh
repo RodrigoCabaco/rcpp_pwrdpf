11d8125c6eba7983289e5cb23321a6d9e49eacd6
Rodrigo Cabaço <42916568+RodrigoCabaco@users.noreply.github.com>
2020-10-20 16:48:29 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -5,16 +5,11 @@ sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
 sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
 sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
 
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-3.1
-
-
-  sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y aspnetcore-runtime-3.1
+sudo apt-get update
+sudo apt-get install -y apt-transport-https && sudo apt-get update && sudo apt-get install -y dotnet-sdk-3.1
+sudo apt-get install -y apt-transport-https &&
+sudo apt-get update &&
+sudo apt-get install -y aspnetcore-runtime-3.1
 
 sudo apt-get install -y dotnet-runtime-3.1
 
